cc61f5be2b48d79c6f6ff83c60d827e8acef266b
David Yatom <dyh@microfocus.com>
2018-09-16 13:10:01 +0300

    add infra

---
+++ BuildingMgmt/Models/BuildingMgmtContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System.Configuration;
+
+namespace BuildingMgmt.Models
+{
+    public partial class BuildingMgmtContext : DbContext
+    {
+        public BuildingMgmtContext()
+        {
+        }
+
+        public BuildingMgmtContext(DbContextOptions<BuildingMgmtContext> options)
+            : base(options)
+        {
+        }
+
+        // Unable to generate entity type for table 'dbo.SiteUsers'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["BuildingMgmt"].ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
